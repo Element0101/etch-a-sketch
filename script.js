f100ea04c33c7b6a661f9bfa67a2c0e442c6e29d
FreedomOutlines <147484609+Element0101@users.noreply.github.com>
2026-01-28 16:57:59 +0100

    Cumtomozing the number of grids based on user input, improve ui and game logic

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-let numberOfDivs = 16 * 16;
+let numberOfDivs = 64 * 64;
+addGrid(numberOfDivs);
 
-const container = document.querySelector(".container");
-const totalHeight = container.offsetHeight;
-const totalWidth = container.offsetWidth;
-const divHeight = totalHeight / 16 - 2;
-const divWidth = totalWidth / 16 - 2;
-for (let i = 0; i < numberOfDivs; i++) {
-    const div = document.createElement("div");
-    
-    div.style.border = "1px, black, solid";
-    div.style.height = divHeight + "px";
-    div.style.width = divWidth + "px";
-    div.style.margin = "0px";
-    div.style.padding = "0px";
-    
-    container.appendChild(div);
+function addGrid (numberOfSquares)  {
+    const container = document.querySelector(".container");
+    const totalHeight = container.offsetHeight;
+    const totalWidth = container.offsetWidth;
+    const divHeight = totalHeight / (Math.sqrt(numberOfSquares));
+    const divWidth = totalWidth / (Math.sqrt(numberOfSquares));
+    for (let i = 0; i < numberOfSquares; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid");
+        div.style.height = divHeight + "px";
+        div.style.width = divWidth + "px";
+        div.style.margin = "0px";
+        div.style.padding = "0px";
+        
+        container.appendChild(div);
+    }
+    const divs = document.querySelectorAll(".grid")
 
-}
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].addEventListener("mouseover", (e) => {
+            divs[i].style.backgroundColor = "blue";
+        });
+    } }
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let squaresNumber = prompt("Add number of squares per side (Max input 100!))");
+    if (squaresNumber <= 100) {
+        squaresNumber = squaresNumber ** 2;
+        const divs = document.querySelectorAll(".grid");
+        divs.forEach(element => element.remove());
+        addGrid(squaresNumber);
+    }else {
+        alert("Max input not bigger than 100!");
+    }
+})
